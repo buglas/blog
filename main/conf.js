5b5b01d38a550f7577da3dcef654edff29379edd
buglas <liwei1051904257@gmail.com>
2020-09-25 22:27:32 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main/build.js
    #	mds/canvas/图形选择/01-图形选择-svg.md
    #	mds/canvas/图形选择/02-图形选择-isPointInPath(x,y).md
    #	mds/canvas/图形选择/03-图形选择-图形模块化.md
    #	mds/canvas/图形选择/04-图形选择-物质不易.md
    #	mds/canvas/图形选择/05-图形选择-网格选择.md
    #	mds/canvas/图形选择/06-图形选择-多边形网格化.md

--- main/conf.js
+++ main/conf.js
@@ -3,24 +3,24 @@ module.exports=function(accessKeyId='',accessKeySecret=''){
         "enter": "./mds",
         "out": "./dist",
         "menu": {
-        "st":"./menu/st.json",
+            "st":"./menu/st.json",
             "mds":"./menu/mds.json"
-    },
+        },
         "ossPathSt": "https://blog-st.oss-cn-beijing.aliyuncs.com/",
         "ossPathMds": "https://blog-mds.oss-cn-beijing.aliyuncs.com/",
         "bucketSt": {
-        "region": "oss-cn-beijing",
+            "region": "oss-cn-beijing",
             "accessKeyId": accessKeyId,
             "accessKeySecret": accessKeySecret,
             "endPoint": "oss-cn-beijing.aliyuncs.com",
             "bucket": "blog-st"
-    },
+        },
         "bucketMds": {
-        "region": "oss-cn-beijing",
+            "region": "oss-cn-beijing",
             "accessKeyId": accessKeyId,
             "accessKeySecret": accessKeySecret,
             "endPoint": "oss-cn-beijing.aliyuncs.com",
             "bucket": "blog-mds"
-    }
+        }
     }
 }
